f46da1a892804c1df135a379c4795f3cb5c8b9bd
Bost <thebost@gmail.com>
2016-08-28 13:39:07 +0200

    org.clojure/core.logic "0.8.10"

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Set of exercises (koans) to learn monads in clojure"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [koan-engine "0.2.3"]
-                 [org.clojure/algo.monads "0.1.6"]]
+                 [org.clojure/algo.monads "0.1.6"]
+                 [org.clojure/core.logic "0.8.10"]]
   :dev-dependencies [[lein-koan "0.1.2"]]
   :plugins [[lein-koan "0.1.2"]])
