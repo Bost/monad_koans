33a7207ac195ce4ea84375198ec2f26e684a3c9d
Bost <thebost@gmail.com>
2019-07-26 17:52:18 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject monad-koans "1.0.0"
   :description "Set of exercises (koans) to learn monads in clojure"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.rpl/specter "0.13.0"]
-                 [koan-engine "0.2.3"]
-                 [org.clojure/algo.monads "0.1.6"]
-                 [org.clojure/core.logic "0.8.11"]]
+  :dependencies
+  [[org.clojure/clojure "1.10.1"]
+   [com.rpl/specter "1.1.2"]
+   [koan-engine "0.2.5"]
+   [org.clojure/algo.monads "0.1.6"]
+   [org.clojure/core.logic "0.8.11"]]
   :dev-dependencies [[lein-koan "0.1.3"]]
-  :plugins [[lein-koan "0.1.3"]])
+  :plugins [[lein-koan "0.1.5"]])
