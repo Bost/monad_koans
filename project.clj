f03869e705028ae885bea177c7fdca355f77c8a2
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2020-05-09 21:36:05 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "Set of exercises (koans) to learn monads in clojure"
   :dependencies
   [[org.clojure/clojure "1.10.1"]
-   [com.rpl/specter "1.1.2"]
+   [com.rpl/specter "1.1.3"]
    [koan-engine "0.2.5"]
    [org.clojure/algo.monads "0.1.6"]
-   [org.clojure/core.logic "0.8.11"]]
+   [org.clojure/core.logic "1.0.0"]]
   :dev-dependencies [[lein-koan "0.1.3"]]
   :plugins [[lein-koan "0.1.5"]])
